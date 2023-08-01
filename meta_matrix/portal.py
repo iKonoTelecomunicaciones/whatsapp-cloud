f7a0a06b90cd5cdf3989bd57ea671a98e28e3e80
egalvis <egalvis@ikono.com.co>
2023-08-01 17:35:52 -0500

    Correctly initialize the meta portals

--- meta_matrix/portal.py
+++ meta_matrix/portal.py
@@ -64,14 +64,14 @@ class Portal(DBPortal, BasePortal):
         self,
         ps_id: str,
         app_page_id: str,
-        room_id: Optional[RoomID] = None,
+        mxid: Optional[RoomID] = None,
         relay_user_id: UserID | None = None,
     ) -> None:
-        super().__init__(ps_id, app_page_id, room_id, relay_user_id)
+        super().__init__(ps_id, app_page_id, mxid, relay_user_id)
         BasePortal.__init__(self)
         self._create_room_lock = Lock()
         self._send_lock = Lock()
-        self.log = self.log.getChild(self.ps_id or self.room_id)
+        self.log = self.log.getChild(self.ps_id or self.mxid)
         self._main_intent = None
         self._relay_user = None
         self.error_codes = self.config["meta.error_codes"]
@@ -114,13 +114,13 @@ class Portal(DBPortal, BasePortal):
         if html is not None:
             content.format = Format.HTML
             content.formatted_body = html
-        return self.main_intent.send_message(self.room_id, content)
+        return self.main_intent.send_message(self.mxid, content)
 
     async def create_matrix_room(
         self, source: User, sender: MetaMessageSender, app_origin: str
     ) -> RoomID:
-        if self.room_id:
-            return self.room_id
+        if self.mxid:
+            return self.mxid
         async with self._create_room_lock:
             try:
                 self.ps_id = sender.id
@@ -170,7 +170,7 @@ class Portal(DBPortal, BasePortal):
 
         if not self.config["bridge.federate_rooms"]:
             creation_content["m.federate"] = False
-        self.room_id = await self.main_intent.create_room(
+        self.mxid = await self.main_intent.create_room(
             name=room_name_template.format(**room_name_variables),
             is_direct=self.is_direct,
             initial_state=initial_state,
@@ -183,39 +183,39 @@ class Portal(DBPortal, BasePortal):
             #      use the power level event in initial_state
             power_level_override={"users": {self.main_intent.mxid: 9001}},
         )
-        if not self.room_id:
+        self.relay_user_id = source.mxid
+
+        if not self.mxid:
             raise Exception("Failed to create room: no mxid returned")
 
-        self.log.debug(self.ps_id)
         puppet: Puppet = await Puppet.get_by_ps_id(self.ps_id, app_page_id=self.app_page_id)
         puppet.display_name = f"User {self.ps_id}"
         await self.main_intent.invite_user(
-            self.room_id, puppet.mxid, extra_content=self._get_invite_content(puppet)
+            self.mxid, puppet.mxid, extra_content=self._get_invite_content(puppet)
         )
         if puppet:
             try:
-                await puppet.intent.join_room_by_id(self.room_id)
+                await puppet.intent.join_room_by_id(self.mxid)
             except MatrixError:
                 self.log.debug(
                     "Failed to join custom puppet into newly created portal", exc_info=True
                 )
         await self.update()
-        meta_user_info = await self.meta_client.get_user_data(self.ps_id)
-        await puppet.update_info(meta_user_info)
-        self.log.debug(f"Matrix room created: {self.room_id}")
-        self.by_mxid[self.room_id] = self
-        return self.room_id
+        await puppet.update_info(creator_info)
+        self.log.debug(f"Matrix room created: {self.mxid}")
+        self.by_mxid[self.mxid] = self
+        return self.mxid
 
     async def handle_matrix_leave(self, user: User) -> None:
         if self.is_direct:
-            self.log.info(f"{user.mxid} left private chat portal with {self.chat_id}")
-            if f"{user.gs_app}-{user.phone}" == self.chat_id:
+            self.log.info(f"{user.mxid} left private chat portal with {self.mxid}")
+            if Puppet.get_by_mxid(user.mxid):
                 self.log.info(
                     f"{user.mxid} was the recipient of this portal. Cleaning up and deleting..."
                 )
                 await self.cleanup_and_delete()
         else:
-            self.log.debug(f"{user.mxid} left portal to {self.chat_id}")
+            self.log.debug(f"{user.mxid} left portal to {self.mxid}")
 
     def _get_invite_content(self, double_puppet: Puppet | None) -> dict[str, Any]:
         invite_content = {}
@@ -270,9 +270,9 @@ class Portal(DBPortal, BasePortal):
         }
 
     async def delete(self) -> None:
-        await DBMessage.delete_all(self.room_id)
-        self.by_mxid.pop(self.room_id, None)
-        self.room_id = None
+        await DBMessage.delete_all(self.mxid)
+        self.by_mxid.pop(self.mxid, None)
+        self.mxid = None
         await self.update()
 
     async def get_dm_puppet(self) -> Puppet | None:
@@ -355,9 +355,7 @@ class Portal(DBPortal, BasePortal):
                 )
 
                 # Send the message to Matrix
-                has_been_sent = await self.main_intent.send_message(
-                    self.room_id, content_attachment
-                )
+                has_been_sent = await self.main_intent.send_message(self.mxid, content_attachment)
 
         elif message.entry.messaging.message and message.entry.messaging.message.reply_to:
             mgs_id = message.entry.messaging.message.reply_to.mid
@@ -405,7 +403,7 @@ class Portal(DBPortal, BasePortal):
 
                 content.external_url = content.external_url
                 # Send the message to Matrix
-                has_been_sent = await self.main_intent.send_message(self.room_id, content)
+                has_been_sent = await self.main_intent.send_message(self.mxid, content)
             else:
                 # if the reply message does not exist, the message is sent as a normal message
                 # Create the content of the message media for send to Matrix
@@ -417,14 +415,12 @@ class Portal(DBPortal, BasePortal):
                 )
 
                 # Send the message to Matrix
-                has_been_sent = await self.main_intent.send_message(
-                    self.room_id, content_attachment
-                )
+                has_been_sent = await self.main_intent.send_message(self.mxid, content_attachment)
 
         puppet: Puppet = await self.get_dm_puppet()
         msg = DBMessage(
             event_mxid=has_been_sent,
-            room_id=self.room_id,
+            room_id=self.mxid,
             ps_id=self.ps_id,
             sender=puppet.mxid,
             meta_message_id=message.entry.messaging.message.mid,
@@ -437,16 +433,16 @@ class Portal(DBPortal, BasePortal):
             return
 
     async def handle_meta_status(self, status_event: MetaStatusEvent) -> None:
-        if not self.room_id:
+        if not self.mxid:
             return
 
         async with self._send_lock:
-            msg = await DBMessage.get_last_message(self.room_id)
+            msg = await DBMessage.get_last_message(self.mxid)
             if status_event.entry.messaging.delivery:
                 pass
             elif status_event.entry.messaging.read:
                 if msg:
-                    await self.main_intent.mark_read(self.room_id, msg.event_mxid)
+                    await self.main_intent.mark_read(self.mxid, msg.event_mxid)
                 else:
                     self.log.debug(f"Ignoring the null message")
 
@@ -477,7 +473,7 @@ class Portal(DBPortal, BasePortal):
             await self.apply_relay_message_format(orig_sender, message)
 
         if message.get_reply_to():
-            await DBMessage.get_by_mxid(message.get_reply_to(), self.room_id)
+            await DBMessage.get_by_mxid(message.get_reply_to(), self.mxid)
 
         if message.msgtype == MessageType.NOTICE and not self.config["bridge.bridge_notices"]:
             return
@@ -491,7 +487,7 @@ class Portal(DBPortal, BasePortal):
                 text = text = message.body
 
             if message.get_reply_to():
-                reply_message = await DBMessage.get_by_mxid(message.get_reply_to(), self.room_id)
+                reply_message = await DBMessage.get_by_mxid(message.get_reply_to(), self.mxid)
                 aditional_data["reply_to"] = {"mid": reply_message.meta_message_id}
 
             try:
@@ -503,9 +499,6 @@ class Portal(DBPortal, BasePortal):
                 )
             except Exception as error:
                 self.log.error(f"Error sending the message: {error}")
-                await self.main_intent.send_notice(
-                    self.room_id, f"Error sending content: {error_message}"
-                )
                 return
         # If the message is a media message, we send the url of the media message to the Meta API
         elif message.msgtype in (
@@ -519,7 +512,7 @@ class Portal(DBPortal, BasePortal):
             # If the message was reply to another message, we add the message id to the
             # reply_to field
             if message.get_reply_to():
-                reply_message = await DBMessage.get_by_mxid(message.get_reply_to(), self.room_id)
+                reply_message = await DBMessage.get_by_mxid(message.get_reply_to(), self.mxid)
                 aditional_data["reply_to"] = {"mid": reply_message.meta_message_id}
 
             # We get the url of the media message. Message.url is something like mxc://xyz, so we
@@ -541,7 +534,7 @@ class Portal(DBPortal, BasePortal):
                 self.log.error(f"Error sending the attachment data: {error}")
                 error_message = error.args[0].get("error", {}).get("message", "")
                 await self.main_intent.send_notice(
-                    self.room_id, f"Error sending content: {error_message}"
+                    self.mxid, f"Error sending content: {error_message}"
                 )
                 return
 
@@ -556,7 +549,7 @@ class Portal(DBPortal, BasePortal):
         self.log.debug(f"Meta send response: {response}")
         await DBMessage(
             event_mxid=event_id,
-            room_id=self.room_id,
+            room_id=self.mxid,
             ps_id=self.ps_id,
             sender=sender.mxid,
             meta_message_id=MetaMessageID(response.get("message_id")),
@@ -568,8 +561,8 @@ class Portal(DBPortal, BasePortal):
 
     async def postinit(self) -> None:
         await self.init_meta_client
-        if self.room_id:
-            self.by_mxid[self.room_id] = self
+        if self.mxid:
+            self.by_mxid[self.mxid] = self
 
         if self.ps_id:
             self.by_ps_id[self.ps_id] = self
