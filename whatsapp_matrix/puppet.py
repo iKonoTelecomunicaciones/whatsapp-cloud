eb6b454836eb95dcd673d1668ca010759ead0e3e
dflorez <dflorez@ikono.com.co>
2025-09-04 10:42:36 -0500

    fix(Fix): :bug: Added validation to avoid duplicated messages when tries to update name

--- whatsapp_matrix/puppet.py
+++ whatsapp_matrix/puppet.py
@@ -128,18 +128,19 @@ class Puppet(DBPuppet, BasePuppet):
         # If the puppet already exists, validate if the name is the same as the one in the database,
         # like user_name (WB), because the _get_displayname function will return user_name (WB) (WB)
         # and the display_name will be updated.
-        if info.profile.name == self.display_name:
-            return False
-        name = self._get_displayname(info)
-        if name != self.display_name:
-            self.display_name = name
-            try:
-                await self.default_mxid_intent.set_displayname(self.display_name)
-                self.name_set = True
-            except Exception:
-                self.log.exception("Failed to update displayname")
-                self.name_set = False
-            return True
+        if info.get("profile"):
+            if info.profile.name == self.display_name:
+                return False
+            name = self._get_displayname(info)
+            if name != self.display_name:
+                self.display_name = name
+                try:
+                    await self.default_mxid_intent.set_displayname(self.display_name)
+                    self.name_set = True
+                except Exception:
+                    self.log.exception("Failed to update displayname")
+                    self.name_set = False
+                return True
         return False
 
     @classmethod
