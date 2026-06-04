d6512401a85b9072d853cfb17ccb788d0a5570f8
@jcardenas4773819 <jcardenas@ikono.com.co>
2026-06-04 16:52:17 -0500

    refactor: :recycle: Refactor puppet to manage bsuid and username

--- whatsapp_matrix/db/puppet.py
+++ whatsapp_matrix/db/puppet.py
@@ -4,9 +4,8 @@ from typing import TYPE_CHECKING, ClassVar
 
 import asyncpg
 from attr import dataclass
-from mautrix.types import SyncToken, UserID
+from mautrix.types import UserID
 from mautrix.util.async_db import Database
-from yarl import URL
 
 fake_db = Database.create("") if TYPE_CHECKING else None
 
@@ -16,32 +15,21 @@ class Puppet:
     db: ClassVar[Database] = fake_db
 
     phone_id: str
-    app_business_id: str
     display_name: str | None
-    is_registered: bool
     custom_mxid: UserID | None
-    access_token: str | None
-    next_batch: SyncToken | None
-    base_url: URL | None
+    username: str | None
+    id: int | None = None
 
     @property
     def _values(self):
         return (
             self.phone_id,
-            self.app_business_id,
             self.display_name,
-            self.is_registered,
             self.custom_mxid,
-            self.access_token,
-            self.next_batch,
-            str(self.base_url) if self.base_url else None,
+            self.username,
         )
 
-    # TODO: Implement this property in the methods to which it applies
-    _columns = (
-        "phone_id, app_business_id, display_name, is_registered, custom_mxid, access_token, "
-        "next_batch, base_url"
-    )
+    _columns = "phone_id, display_name, custom_mxid, username"
 
     @classmethod
     def _from_row(cls, row: asyncpg.Record) -> Puppet:
@@ -49,27 +37,47 @@ class Puppet:
 
     async def insert(self) -> None:
         q = """
-            INSERT INTO puppet (phone_id, app_business_id, display_name, is_registered,
-            custom_mxid, access_token, next_batch, base_url)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            INSERT INTO puppet (phone_id, display_name, custom_mxid, username)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id
         """
-        await self.db.execute(q, *self._values)
+        self.id = await self.db.fetchval(q, *self._values)
 
     async def update(self) -> None:
         q = """
-            UPDATE puppet SET phone_id=$1, app_business_id=$2, display_name=$3, is_registered=$4,
-            custom_mxid=$5, access_token=$6, next_batch=$7, base_url=$8  WHERE phone_id=$1
+            UPDATE puppet SET phone_id=$1, display_name=$2, username=$4
+            WHERE custom_mxid=$3
         """
         await self.db.execute(q, *self._values)
 
     @classmethod
-    async def get_by_phone_id(cls, phone_id: str) -> Puppet | None:
+    async def get_by_identifier(cls, identifier: str) -> Puppet | None:
         q = """
-            SELECT phone_id, app_business_id, display_name, is_registered, custom_mxid,
-            access_token, next_batch, base_url
-            FROM puppet WHERE phone_id=$1
+            SELECT id, phone_id, display_name, custom_mxid, username
+            FROM puppet WHERE custom_mxid = $1
         """
-        row = await cls.db.fetchrow(q, phone_id)
+        row = await cls.db.fetchrow(q, identifier)
+        if not row:
+            return None
+        return cls._from_row(row)
+
+    @classmethod
+    async def get_by_id(cls, id: int) -> Puppet | None:
+        """
+        Get the puppet by id.
+
+        Parameters
+        ----------
+        id : int
+            The id of the puppet.
+
+        Returns
+        -------
+        Puppet | None
+            The puppet with the given id, or None if it doesn't exist.
+        """
+        q = f"SELECT id, {cls._columns} FROM puppet WHERE id = $1"
+        row = await cls.db.fetchrow(q, id)
         if not row:
             return None
         return cls._from_row(row)
@@ -77,8 +85,7 @@ class Puppet:
     @classmethod
     async def get_by_custom_mxid(cls, mxid: UserID) -> Puppet | None:
         q = """
-            SELECT phone_id, app_business_id, display_name, is_registered, custom_mxid,
-            access_token, next_batch, base_url
+            SELECT id, phone_id, display_name, custom_mxid, username
             FROM puppet WHERE custom_mxid=$1
         """
         row = await cls.db.fetchrow(q, mxid)
@@ -89,8 +96,7 @@ class Puppet:
     @classmethod
     async def all_with_custom_mxid(cls) -> list[Puppet]:
         q = """
-            SELECT  phone_id, app_business_id, display_name, is_registered, custom_mxid,
-            access_token, next_batch, base_url
+            SELECT id, phone_id, display_name, custom_mxid, username
             FROM puppet WHERE custom_mxid IS NOT NULL
         """
         rows = await cls.db.fetch(q)
