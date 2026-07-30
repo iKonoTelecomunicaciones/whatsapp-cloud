43952e500e93ef2579e1bd47fa0c2d53c5adc333
Juan Manuel Cárdenas <138890925+jcardenas3@users.noreply.github.com>
2026-07-30 09:52:26 -0500

    fix: :bug: Fix error getting the username (#183)

--- whatsapp/data.py
+++ whatsapp/data.py
@@ -865,6 +865,7 @@ class WhatsappProfile(SerializableAttrs):
     """
 
     name: str = ib(metadata={"json": "name"}, default="")
+    username: str = ib(metadata={"json": "username"}, default="")
 
 
 @dataclass
