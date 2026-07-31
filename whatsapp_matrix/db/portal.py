c22ff8696c995ed3fb3990652b3271fb33c2f7ab
Juan Manuel Cárdenas <138890925+jcardenas3@users.noreply.github.com>
2026-07-31 11:09:25 -0500

    Allow use of bsuid (#185)
    * refactor: :recycle: Changed the database to support bsuid (#157)
    
    * refactor: :recycle: Changed the database to support bsuid
    
    * chore: :art: Improved SQL syntax
    
    * revert: :rewind: Revert the drop of the phone id column in the portal table
    
    * refactor: :recycle: Deleted not null constraints of phone_id in puppet and portal
    
    * chore: :art: Add blank line to pass test
    
    * refactor: :recycle: Refactor puppet to manage bsuid and username (#160)
    
    * refactor: :recycle: Refactor puppet to manage bsuid and username
    
    * chore: :art: Isort code to pass tests
    
    * refactor: :recycle: Use a context manager to block portals (#154)
    
    * refactor: :recycle: Use a context manager to block portals
    
    * fix: :bug: Fix hug of death
    
    * style: :art: Format code to pass tests
    
    * Manage bsuid in portal (#161)
    
    * refactor: :recycle: Refactor puppet to manage bsuid and username
    
    * refactor: :recycle: Allow portal to use bsuid
    
    * chore: :art: Format code to pass tests
    
    * style: :art: Format code to pass tests
    
    * chore: :fire: Remove unused codee
    
    * 156 manage bsuid to handle events (#162)
    
    * refactor: :recycle: Refactor puppet to manage bsuid and username
    
    * refactor: :recycle: Allow portal to use bsuid
    
    * chore: :art: Isort code to pass tests
    
    * chore: :art: Format code to pass tests
    
    * refactor: :recycle: Added way to send messages using BSUID
    
    * chore: :mute: Remove critical log
    
    * fix: :bug: Fix some bugs creating portal and puppet
    
    * chore: :art: Format code to pass tests
    
    * revert: :rewind: Revert deletion of the get_portal_with function
    
    * fix: :bug: Fix the bug that occurs when getting a puppet for a user with a username
    
    * style: :art: Format code to pass tests
    
    * fix: :bug: Fix error getting username

--- whatsapp_matrix/db/portal.py
+++ whatsapp_matrix/db/portal.py
@@ -14,10 +14,13 @@ fake_db = Database.create("") if TYPE_CHECKING else None
 class Portal:
     db: ClassVar[Database] = fake_db
 
-    phone_id: str
     app_business_id: str
     mxid: RoomID | None
     relay_user_id: UserID | None
+    phone_id: str | None = None
+    bsuid: str | None = None
+    puppet_id: int | None = None
+    id: int | None = None
 
     @property
     def _values(self):
@@ -26,37 +29,62 @@ class Portal:
             self.app_business_id,
             self.mxid,
             self.relay_user_id,
+            self.bsuid,
+            self.puppet_id,
         )
 
-    _columns = "phone_id, app_business_id, mxid, relay_user_id"
+    _columns = "phone_id, app_business_id, mxid, relay_user_id, bsuid, puppet_id"
 
     @classmethod
     def _from_row(cls, row: asyncpg.Record) -> Portal:
         return cls(**row)
 
     async def insert(self) -> None:
-        q = f"INSERT INTO portal ({self._columns}) VALUES ($1, $2, $3, $4)"
-        await self.db.execute(q, *self._values)
+        q = f"INSERT INTO portal ({self._columns}) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+        self.id = await self.db.fetchval(q, *self._values)
 
     async def update(self) -> None:
         q = """
             UPDATE portal
-            SET phone_id=$1, app_business_id= $2, mxid=$3, relay_user_id=$4
-            WHERE phone_id=$1 AND app_business_id=$2
+            SET phone_id=$1, app_business_id=$2, mxid=$3, relay_user_id=$4, bsuid=$5, puppet_id=$6
+            WHERE (phone_id=$1 OR bsuid=$5) AND app_business_id=$2
         """
         await self.db.execute(q, *self._values)
 
     @classmethod
     async def get_by_phone_id(cls, phone_id: str, app_business_id: str) -> Optional["Portal"]:
-        q = f"SELECT {cls._columns} FROM portal WHERE phone_id=$1 AND app_business_id=$2"
+        q = f"SELECT id, {cls._columns} FROM portal WHERE phone_id=$1 AND app_business_id=$2"
         row = await cls.db.fetchrow(q, phone_id, app_business_id)
         if not row:
             return None
         return cls._from_row(row)
 
     @classmethod
+    async def get_by_identifier(
+        cls, phone_id: str, bsuid: str | None, app_business_id: str
+    ) -> "Portal" | None:
+        q = (
+            f"SELECT id, {cls._columns} FROM portal WHERE (phone_id=$1 OR bsuid=$2)"
+            " AND app_business_id=$3"
+        )
+        row = await cls.db.fetchrow(q, phone_id, bsuid, app_business_id)
+        if not row:
+            return None
+        return cls._from_row(row)
+
+    @classmethod
+    async def get_by_puppet_id_and_business_id(
+        cls, puppet_id: str, app_business_id: str
+    ) -> "Portal" | None:
+        q = f"SELECT id, {cls._columns} FROM portal WHERE puppet_id=$1 AND app_business_id=$2"
+        row = await cls.db.fetchrow(q, puppet_id, app_business_id)
+        if not row:
+            return None
+        return cls._from_row(row)
+
+    @classmethod
     async def get_by_mxid(cls, mxid: RoomID) -> Optional["Portal"]:
-        q = f"SELECT {cls._columns} FROM portal WHERE mxid=$1"
+        q = f"SELECT id, {cls._columns} FROM portal WHERE mxid=$1"
         row = await cls.db.fetchrow(q, mxid)
         if not row:
             return None
@@ -64,6 +92,6 @@ class Portal:
 
     @classmethod
     async def all_with_room(cls) -> list[Portal]:
-        q = f"SELECT {cls._columns} FROM portal WHERE mxid IS NOT NULL"
+        q = f"SELECT id, {cls._columns} FROM portal WHERE mxid IS NOT NULL"
         rows = await cls.db.fetch(q)
         return [cls._from_row(row) for row in rows]
