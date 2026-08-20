7ae5d2b35d43c4f7db1a26d9e6b2e32cf88cab25
@jcardenas4773819 <jcardenas@ikono.com.co>
2026-08-20 11:30:51 -0500

    fix: :bug: Fix error updating puppets with username

--- whatsapp_matrix/portal.py
+++ whatsapp_matrix/portal.py
@@ -429,8 +429,8 @@ class Portal(DBPortal, BasePortal):
             return
 
         self.log.info(f"Deleting duplicate puppet {puppet.id}")
-        old_portal: Portal = await Portal.get_by_puppet_and_business_id(
-            puppet.id, self.app_business_id
+        old_portal: Portal | None = cast(
+            Portal, await super().get_by_puppet_id_and_business_id(puppet.id, self.app_business_id)
         )
 
         self.log.info(f"Getting old portal: {old_portal.mxid if old_portal else None}")
@@ -438,13 +438,20 @@ class Portal(DBPortal, BasePortal):
         if old_portal and old_portal.id != self.id:
             self.log.info(f"Deleting old portal {old_portal.mxid}")
             try:
-                await old_portal.main_intent.set_room_name(
+                await puppet.default_mxid_intent.set_room_name(
                     old_portal.mxid, f"{puppet.display_name} (old)"
                 )
             except IntentError as e:
                 self.log.exception(f"Failed to set room name: {e}")
 
             await old_portal.delete()
+        portals_to_update = await Portal.get_portals_by_puppet_id(puppet.id)
+
+        if portals_to_update:
+            for portal in portals_to_update:
+                portal.puppet_id = self.puppet_id
+                await portal.update()
+                await portal.postinit()
 
         self.log.info(f"Deleting puppet {puppet.id}")
         await puppet.delete()
@@ -666,8 +673,18 @@ class Portal(DBPortal, BasePortal):
                     await puppet.update()
 
             if not puppet.bsuid and self.bsuid:
-                puppet.bsuid = self.bsuid
-                await puppet.update()
+                try:
+                    puppet.bsuid = self.bsuid
+                    await puppet.update()
+                except UniqueViolationError:
+                    self.log.error(
+                        f"Failed to update puppet for phone {self.phone_id} and bsuid "
+                        f"{self.bsuid}, searching for existing puppet"
+                    )
+                    await self.delete_duplicate_puppet()
+                    puppet = await Puppet.get_by_id(self.puppet_id)
+                    puppet.bsuid = self.bsuid
+                    await puppet.update()
 
             return puppet
 
