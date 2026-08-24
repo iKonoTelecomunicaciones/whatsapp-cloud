4d10f3a040b786ac890b8dd8f701dbcd029494e5
@jcardenas4773819 <jcardenas@ikono.com.co>
2026-08-24 16:57:14 -0500

    revert: :rewind: Revert some changes in v7 migration

--- whatsapp_matrix/db/upgrade.py
+++ whatsapp_matrix/db/upgrade.py
@@ -243,4 +243,23 @@ async def upgrade_v6(conn: Connection) -> None:
 
 @upgrade_table.register(description="Add pin column; add ON UPDATE CASCADE to FK constraints")
 async def upgrade_v7(conn: Connection) -> None:
-    await conn.execute("ALTER TABLE wb_application ADD pin TEXT;")
+    # Add pin column to wb_application table if it doesn't exist
+    await conn.execute("ALTER TABLE wb_application ADD COLUMN IF NOT EXISTS pin TEXT;")
+
+    await conn.execute(
+        "ALTER TABLE portal DROP CONSTRAINT FK_portal_wb_application_app_business_id"
+    )
+    await conn.execute(
+        "ALTER TABLE matrix_user DROP CONSTRAINT FK_matrix_user_wb_application_app_business_id"
+    )
+
+    await conn.execute(
+        """ALTER TABLE portal ADD CONSTRAINT FK_portal_wb_application_app_business_id
+        FOREIGN KEY (app_business_id) REFERENCES wb_application (business_id)
+        ON UPDATE CASCADE"""
+    )
+    await conn.execute(
+        """ALTER TABLE matrix_user ADD CONSTRAINT FK_matrix_user_wb_application_app_business_id
+        FOREIGN KEY (app_business_id) REFERENCES wb_application (business_id)
+        ON UPDATE CASCADE"""
+    )
